8cd833c5b6849bf1b1c0c168e2875e549f42e90c
ksetetemela <khobatha.setetemela@gmail.com>
2023-04-20 11:57:43 +0200

    debug

--- src/main/resources/static/js/initAutomation.js
+++ src/main/resources/static/js/initAutomation.js
@@ -47,7 +47,7 @@ function renderDHISSchedules(){
 							'<td>' + object.frequency + '</td>' +
 							'<td>' + object.lastRun + '</td>' +
 							'<td>' + object.status + '</td>' +
-							'<td>' + object.targetDate + '</td>';
+							'<td></td>';
 			if(object.reportId==1){
 				tr.innerHTML =tempHTML+
 							"<td>"+
