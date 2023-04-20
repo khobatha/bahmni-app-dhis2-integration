eb00f353f62c3d18ab9c84b08d74a3c20e813d89
ksetetemela <khobatha.setetemela@gmail.com>
2023-04-20 10:40:55 +0200

    log schedule target date

--- src/main/java/com/possible/dhis2int/scheduler/DHISIntegratorScheduler.java
+++ src/main/java/com/possible/dhis2int/scheduler/DHISIntegratorScheduler.java
@@ -116,6 +116,7 @@ public class DHISIntegratorScheduler {
 				schedule.setEnabled(Integer.parseInt(row.get(4)) == 1 ? true : false);
 				schedule.setLastRun(row.get(5));
 				schedule.setTargetDate(LocalDate.parse(row.get(6),formatter));
+				logger.info("Target date is "+LocalDate.parse(row.get(6),formatter));
 				schedule.setStatus(row.get(7));
 				list.add(schedule);
 			}
