953e440d5784fd1585ebf86de254fd7926774182
ksetetemela <khobatha.setetemela@gmail.com>
2023-05-11 04:58:44 +0200

    set Statement.RETURN_GENERATED_KEYS=true

--- src/main/java/com/possible/dhis2int/db/DatabaseDriver.java
+++ src/main/java/com/possible/dhis2int/db/DatabaseDriver.java
@@ -125,7 +125,7 @@ public class DatabaseDriver {
 		try {
 			connection = DriverManager.getConnection(properties.openmrsDBUrl);
 			PreparedStatement ps = connection.prepareStatement(
-					"INSERT INTO dhis2_schedules (report_name,report_id,enabled,created_by,created_date) VALUES (?, ?, ?, ?, ?)");
+					"INSERT INTO dhis2_schedules (report_name,report_id,enabled,created_by,created_date) VALUES (?, ?, ?, ?, ?)",Statement.RETURN_GENERATED_KEYS);
 
 			ps.setString(1, record.getProgramName());
 			ps.setInt(2, record.getReportId());
