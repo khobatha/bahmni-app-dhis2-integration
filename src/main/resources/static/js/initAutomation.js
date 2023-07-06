f7d80e8623dd786a11ced3a279a9c739e6e43ed3
ksetetemela <khobatha.setetemela@gmail.com>
2023-07-06 14:28:50 +0200

    exclude json pre-parsing

--- src/main/resources/static/js/initAutomation.js
+++ src/main/resources/static/js/initAutomation.js
@@ -214,11 +214,11 @@ function getSchedulePeriods(schedule_id){
 //populate and render list of schedules from db
 function renderDHISSchedules(url){
 
-	var data=getDHISSchedules(url);
+	var schedules=getDHISSchedules(url);
 	//getDHISSchedules(url).then(function(data){
 	console.log('[render hmis program schedules]');
 	console.log('[url ]'+url);
-	console.log('Loaded data is'+data);
+	//console.log('Loaded data is'+data);
 	//alert(data);
 	var clinicalSchedulesTable = document.getElementById('clinical-program-schedules');
 	var pharmacySchedulesTable = document.getElementById('pharmacy-program-schedules');
@@ -226,7 +226,7 @@ function renderDHISSchedules(url){
 	//var schedules=JSON.parse(data);
 	var tempHTML;
 	var tr = document.createElement('tr');
-	/*schedules.forEach(function(object) {
+	schedules.forEach(function(object) {
 		console.log('[renderDHISSchedules] Processing schedule '+object.id);
 		//isMultiPeriodSchedule(object.id);
 		// Call the async function
@@ -285,7 +285,7 @@ function renderDHISSchedules(url){
 		}
 		document.getElementById(object.id).checked= object.enabled;
 	});
-	*/
+
 	//});
 }
 
