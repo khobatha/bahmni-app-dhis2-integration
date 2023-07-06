38c8fbf20a59936511d414b0dd3f182b5d6ded10
ksetetemela <khobatha.setetemela@gmail.com>
2023-07-06 21:44:57 +0200

    pass periods as param

--- src/main/resources/static/js/initAutomation.js
+++ src/main/resources/static/js/initAutomation.js
@@ -158,7 +158,7 @@ function getSchedulePeriods(url) {
 		const cell = document.getElementById(cellId);
 		cell.insertBefore(link,cell.firstChild);
 
-		generateMultiSchedulePeriodsHtml(schedule_id);
+		generateMultiSchedulePeriodsHtml(schedule_id,result);
 		//return true;
 	}
 	} catch (error) {
@@ -166,14 +166,14 @@ function getSchedulePeriods(url) {
 	}
   }
  // 
-function generateMultiSchedulePeriodsHtml(schedule_id){
+async function generateMultiSchedulePeriodsHtml(schedule_id,result){
 	try{
-		var url=`${getPharmSchedulePeriodsUrl}?pharmschedid=${schedule_id}`;
-		console.log('[generateMultiSchedulePeriodsHtml] Welcome');
-		const data=getSchedulePeriods(url);
+		//var url=`${getPharmSchedulePeriodsUrl}?pharmschedid=${schedule_id}`;
+		//console.log('[generateMultiSchedulePeriodsHtml] Welcome');
+		//const data=await getSchedulePeriods(url);
 		//var data='[{"id":62,"dhis2ScheduleId":25,"period":0,"createdBy":"Test","createdDate":null,"startTime":"2023-08-01 00:00:00.0","endTime":"2023-09-01 00:00:00.0","lastRun":null,"status":"Ready","enabled":true},{"id":63,"dhis2ScheduleId":25,"period":1,"createdBy":"Test","createdDate":null,"startTime":"2023-09-01 00:00:00.0","endTime":"2023-10-01 00:00:00.0","lastRun":null,"status":"Ready","enabled":true}]';
-		console.log('[generateMultiSchedulePeriodsHtml] Loaded data is '+data);
-		var periods=JSON.parse(data[0]);
+		console.log('[generateMultiSchedulePeriodsHtml] Loaded data is '+result);
+		var periods=JSON.parse(result[0]);
 		console.log('[generateMultiSchedulePeriodsHtml] Processing period '+periods[0].id);
 		//periods.forEach(function(object){
 		//	console.log('[generateMultiSchedulePeriodsHtml] Processing period '+object.id);
