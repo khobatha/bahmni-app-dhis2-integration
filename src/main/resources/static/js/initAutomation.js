c4b7811922537748a84c15da33c5f1a3cf49524e
ksetetemela <khobatha.setetemela@gmail.com>
2023-07-06 21:48:48 +0200

    test

--- src/main/resources/static/js/initAutomation.js
+++ src/main/resources/static/js/initAutomation.js
@@ -174,7 +174,7 @@ async function generateMultiSchedulePeriodsHtml(schedule_id,result){
 		//var data='[{"id":62,"dhis2ScheduleId":25,"period":0,"createdBy":"Test","createdDate":null,"startTime":"2023-08-01 00:00:00.0","endTime":"2023-09-01 00:00:00.0","lastRun":null,"status":"Ready","enabled":true},{"id":63,"dhis2ScheduleId":25,"period":1,"createdBy":"Test","createdDate":null,"startTime":"2023-09-01 00:00:00.0","endTime":"2023-10-01 00:00:00.0","lastRun":null,"status":"Ready","enabled":true}]';
 		console.log('[generateMultiSchedulePeriodsHtml] Loaded data is '+result);
 		var periods=result[0];//JSON.parse(result[0]);
-		console.log('[generateMultiSchedulePeriodsHtml] Processing period '+periods[0].id);
+		console.log('[generateMultiSchedulePeriodsHtml] Processing period '+periods.id);
 		//periods.forEach(function(object){
 		//	console.log('[generateMultiSchedulePeriodsHtml] Processing period '+object.id);
 		//});
