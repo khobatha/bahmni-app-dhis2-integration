b9498e6c50538c0a1c8e5878b6d48bf2944fafbe
ksetetemela <khobatha.setetemela@gmail.com>
2023-07-06 21:47:24 +0200

    pass parsed periods

--- src/main/resources/static/js/initAutomation.js
+++ src/main/resources/static/js/initAutomation.js
@@ -158,7 +158,7 @@ function getSchedulePeriods(url) {
 		const cell = document.getElementById(cellId);
 		cell.insertBefore(link,cell.firstChild);
 
-		generateMultiSchedulePeriodsHtml(schedule_id,result);
+		generateMultiSchedulePeriodsHtml(schedule_id,periods);
 		//return true;
 	}
 	} catch (error) {
@@ -173,7 +173,7 @@ async function generateMultiSchedulePeriodsHtml(schedule_id,result){
 		//const data=await getSchedulePeriods(url);
 		//var data='[{"id":62,"dhis2ScheduleId":25,"period":0,"createdBy":"Test","createdDate":null,"startTime":"2023-08-01 00:00:00.0","endTime":"2023-09-01 00:00:00.0","lastRun":null,"status":"Ready","enabled":true},{"id":63,"dhis2ScheduleId":25,"period":1,"createdBy":"Test","createdDate":null,"startTime":"2023-09-01 00:00:00.0","endTime":"2023-10-01 00:00:00.0","lastRun":null,"status":"Ready","enabled":true}]';
 		console.log('[generateMultiSchedulePeriodsHtml] Loaded data is '+result);
-		var periods=JSON.parse(result[0]);
+		var periods=result[0];//JSON.parse(result[0]);
 		console.log('[generateMultiSchedulePeriodsHtml] Processing period '+periods[0].id);
 		//periods.forEach(function(object){
 		//	console.log('[generateMultiSchedulePeriodsHtml] Processing period '+object.id);
